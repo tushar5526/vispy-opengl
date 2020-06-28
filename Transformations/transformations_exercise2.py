355fbc6fd95f589ae402cc3017a1fedcbb261913
Tushar <codingid6@gmail.com>
2020-06-28 22:23:33 +0530

    transformations completed

--- Transformations/transformations_exercise2.py
+++ Transformations/transformations_exercise2.py
@@ -60,10 +60,10 @@ class Canvas(app.Canvas):
         self.texture1 = Texture2D(data=io.imread('../Textures/container.jpg',flipVertically=True))
         self.texture2 = Texture2D(data=io.imread('../Textures/smiley.jpg',flipVertically=True))
 
-        #trans stores matrix in glm format
-        #_trans stores matrix in numpy format
-        self.trans = None
-        self._trans = None
+        self.trans1 = None
+        self.trans2 = None
+        self._trans1 = None
+        self._trans2 = None
 
         self.program['a_position'] = self.vertices
         self.program['aTexCoord'] = self.texCoord
@@ -71,28 +71,45 @@ class Canvas(app.Canvas):
         self.program['texture2'] = self.texture2
 
         self.timer = app.Timer('auto', self.on_timer, start=True)
+
+
         self.show()
 
     def on_draw(self, event):
-        gloo.clear([0.2, 0.3, 0.3, 1.0])
+        gloo.clear([0, 0, 0, 0])
+
+        #1st box
+        # Here _trans stores matrix in numpy format and trans stores data in glm format
+        self.trans1 = glm.mat4(1.0)
+        self.trans1 = glm.translate(self.trans1, glm.vec3(0.5, -0.5, 0.0))
+        self.trans1 = glm.rotate(self.trans1, time() - self.startTime, glm.vec3(0.0, 0.0, 1.0))
+        # vispy takes numpy array in m * n matrix form
+        self._trans1 = (np.array(self.trans1.to_list()).astype(np.float32))
+        # reshaping to (m, n) to (1, m*n) to support data input in vispy
+        self._trans1 = self._trans1.reshape((1, self._trans1.shape[0] * self._trans1.shape[1]))
+        self.program['transform'] = self._trans1
         self.program.draw('triangles', self.indices)
+
+
+        #2nd box
+        self.trans2 = glm.mat4(1.0)
+        self.trans2 = glm.translate(self.trans2, glm.vec3(-0.5, 0.5, 0.0))
+        self.trans2 = glm.scale(self.trans2, glm.vec3(sin(time()), sin(time()), sin(time())))
+        # vispy takes numpy array in m * n matrix form
+        self._trans2 = (np.array(self.trans2.to_list()).astype(np.float32))
+        # reshaping to (m, n) to (1, m*n)
+        self._trans2 = self._trans2.reshape((1, self._trans2.shape[0] * self._trans2.shape[1]))
+        self.program['transform'] = self._trans2
+        self.program.draw('triangles', self.indices)
+
+        self.update()
+
 
     def on_resize(self, event):
         gloo.set_viewport(0, 0, *event.size)
 
     def on_timer(self, event):
-        #we can do this in on_draw too, we will use it in exercise2's solution
-        self.trans = glm.mat4(1.0)
-        self.trans = glm.translate(self.trans, glm.vec3(0.5, -.5, .0))
-        self.trans = glm.rotate(self.trans, time() - self.startTime,glm.vec3(0.0, 0.0, 1.0))
-
-        #convering to numpy arrray to support vispy
-        self._trans = (np.array(self.trans.to_list()).astype(np.float32))
-        # reshaping to (m, n) to (1, m*n)
-        self._trans = self._trans.reshape((1, self._trans.shape[0] * self._trans.shape[1]))
-
-        self.program['transform'] = self._trans
-        self.update()
+        pass
 
 
 if __name__ == '__main__':
